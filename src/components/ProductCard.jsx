0b87114d7b7ea8be326aa1cd61479ed266c3f1a6
adiatraa <adiatrahman31@gmail.com>
2024-07-22 12:36:53 +0700

    Change a href to button

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -21,27 +21,28 @@ export default function ProductCard({ product }) {
 
                     <p className="mt-4 text-base font-medium text-gray-900">{product.price}</p>
                     <div className="mt-6 cursor-pointer">
-                        <a
-                            href={product.href}
-                            className="relative flex items-center justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:text-[#1977F1] hover:bg-gray-200"
+                        <button
+                            onClick={() => handleAddToCart(product)}
+                            className="relative flex items-center w-full justify-center rounded-md border border-transparent bg-gray-100 px-8 py-2 text-sm font-medium text-gray-900 hover:text-[#1977F1] hover:bg-gray-200"
                         >
                             Add to Cart
-                        </a>
+                        </button>
                     </div>
                     <div className="mt-6 cursor-pointer flex gap-4">
-                        <a
-                            href={product.href}
+                        <button
+                            onClick={() => handleDelete(product)}
                             className="relative w-1/2 flex items-center justify-center rounded-md border border-transparent bg-red-500 px-8 py-2 text-sm font-medium text-white hover:text-white"
                         >
                             Delete
-                        </a>
-                        <a
-                            href={product.href}
+                        </button>
+                        <button
+                            onClick={() => handleEdit(product)}
                             className="relative w-1/2 flex items-center justify-center rounded-md border border-transparent bg-[#1977F1] px-8 py-2 text-sm font-medium text-white hover:text-white"
                         >
                             Edit
-                        </a>
+                        </button>
                     </div>
+
                 </div>
             </div>
         </div>
