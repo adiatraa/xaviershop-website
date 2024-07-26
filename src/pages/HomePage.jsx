c2f48f4f94d20c14af6b7352d27c195fb77db041
adiatraa <adiatrahman31@gmail.com>
2024-07-26 16:53:08 +0700

    Update left side hero section

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -9,22 +9,30 @@ function HomePage() {
             <div className="bg-red-500 h-[45vw] w-screen flex">
                 <div className="w-1/2 h-[45vw] bg-[#F3F4F6] p-40 flex flex-col gap-12">
                     <div>
-                        <h1 className="font-extrabold text-[84px] leading-tight font-prompt text-black">Grab <br /> Off Smartphone <br /> Collection </h1>
+                        <div className="flex items-center">
+                            <h1 className="font-extrabold z-10 text-[84px] leading-tight font-prompt text-black">Grab</h1>
+                            <div className="h-[80px] w-[165px] bg-[#1977F1] flex items-center justify-center rounded-3xl ml-[-12px] rotate-[-2deg]">
+                                <h1 className="font-extrabold text-[50px] leading-tight font-prompt text-white">50% </h1>
+                            </div>
+                        </div>
+                        <div>
+                            <h1 className="font-extrabold text-[84px] leading-tight font-prompt text-black">Off Smartphone <br /> Collection </h1>
+                        </div>
                     </div>
                     <div>
-                        <p className="text-xl font-montserrat text-black">Lorem ipsum dolor sit amet consectetur. <br /> Eleifend nec morbi tellus vitae leo nunc.</p>
+                        <p className="text-xl font-montserrat text-black">Explore the latest in smartphones, laptops, iPads, and more. Don't miss out on cutting-edge technology and unbeatable deals. <br /> Shop now and elevate your tech experience!</p>
                     </div>
                     <div className="flex gap-12">
                         <div className="h-[140px] w-[140px] bg-white rounded-3xl flex justify-center items-center overflow-hidden" style={{ boxShadow: '0px 10px 15px -3px rgba(0,0,0,0.1)' }}>
-                            <img src={smallDisplay} className="transition-transform duration-300 ease-out hover:scale-105 max-w-full max-h-full" />
+                            <img src={smallDisplay} className="transition-transform duration-300 ease-out hover:scale-125 max-w-full max-h-full" />
                         </div>
                         <div className="flex justify-center items-center">
                             <h1 className="font-semibold text-[40px] leading-tight font-prompt text-black">iPhone 15 Pro <br /> Edition </h1>
                         </div>
                     </div>
                 </div>
-                <div className="w-1/2 h-[45vw] bg-yellow-500">
-
+                <div className="w-1/2 h-[45vw] bg-[#F3F4F6] flex items-center justify-center">
+                    <h1 className="font-extrabold text-[84px] leading-tight font-prompt text-black"> DRAFT SECTION</h1>
                 </div>
             </div>
         </div>
