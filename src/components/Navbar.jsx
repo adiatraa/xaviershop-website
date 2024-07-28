e1c1d9d3d7ddc9cda39489e07ca4c947763e9e06
adiatraa <adiatrahman31@gmail.com>
2024-07-28 13:49:23 +0700

    Move footer as a component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,8 +19,14 @@ export default function Navbar() {
                         <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
                             {/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
                             <a
-                                href="#"
+                                href="/"
                                 className="inline-flex items-center border-b-2 border-[#1977F1] px-1 pt-1 text-sm font-medium text-gray-900"
+                            >
+                                Home
+                            </a>
+                            <a
+                                href="#"
+                                className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
                             >
                                 Products
                             </a>
@@ -29,12 +35,6 @@ export default function Navbar() {
                                 className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
                             >
                                 Cart
-                            </a>
-                            <a
-                                href="#"
-                                className="inline-flex items-center border-b-2 border-transparent px-1 pt-1 text-sm font-medium text-gray-500 hover:border-gray-300 hover:text-gray-700"
-                            >
-                                Order
                             </a>
                         </div>
                     </div>
